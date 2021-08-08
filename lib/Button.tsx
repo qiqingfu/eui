d18f68fbbc8752c63bb7671503eac55101d8e3ae
qiqf <723469354@qq.com>
2021-08-08 12:59:29 +0800

    chore: 新增react、react-dom库

---
+++ lib/Button.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = () => {
+  return <button>按钮</button>;
+};
+
+export default Button;
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,5 @@
-console.log("Hello, World!!");
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "./Button";
+
+ReactDOM.render(<Button />, document.getElementById("root"));
