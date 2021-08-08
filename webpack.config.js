45f87281955931a6bd0ed95d7957bb46e132e933
qiqf <723469354@qq.com>
2021-08-08 13:38:25 +0800

    chore: 添加 externals 优化配置

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: {
     index: "./lib/index.tsx",
   },
@@ -13,10 +13,6 @@ module.exports = {
       name: "Eui",
       type: "umd",
     },
-  },
-  devServer: {
-    port: 9000,
-    host: "localhost",
   },
   // Currently we need to add '.ts' to the resolve.extensions array.
   resolve: {
@@ -38,4 +34,19 @@ module.exports = {
       path: path.resolve(__dirname, "dist"),
     }),
   ],
+  // https://segmentfault.com/a/1190000012113011
+  externals: {
+    react: {
+      root: "React", // 可以通过一个全局变量访问 library（例如，通过 script 标签）
+      commonjs: "react", // 可以将 library 作为一个 CommonJS 模块访问
+      commonjs2: "react", // 和上面的类似，但导出的是 module.exports.default
+      amd: "react",
+    },
+    "react-dom": {
+      root: "ReactDOM",
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "react-dom",
+    },
+  },
 };
