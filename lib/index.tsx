f8619cf09b106580900958d4fa96cca8ef1ec4b0
qiqf <723469354@qq.com>
2021-08-10 23:35:22 +0800

    chore: 将awesome-typescript-loader 替换为 ts-loader

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Button from "./Button";
+import Icon from "./Icon"
 
-ReactDOM.render(<Button />, document.getElementById("root"));
+ReactDOM.render(<Icon name="delete"/>, document.getElementById("root"));
--- webpack.base.js
+++ webpack.base.js
@@ -21,7 +21,7 @@ module.exports = {
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        loader: "awesome-typescript-loader",
+        loader: "ts-loader",
       },
     ],
   },
