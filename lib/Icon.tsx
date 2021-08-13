64eeacead1c4a6ee46389439dadced7e979dd1c2
qiqingfu <723469354@qq.com>
2021-08-13 17:00:12 +0800

    feat: Icon 组件新增color和size用于设置 style 样式

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -1,18 +1,35 @@
 import React from "react"
+import classNames from "classnames";
 import "./icons"
 import "./styles/icon.scss"
 
-interface IconProps {
-	name: string
+export interface IconProps<T = SVGElement> extends React.SVGAttributes<T> {
+  name: string,
+  color?: string,
+  size?: number
 }
 
-const Icon: React.FunctionComponent<IconProps> = (props) => {
-	return (
-		// https://stackoverflow.com/questions/26815738/svg-use-tag-and-reactjs
-		<svg className="e-ui-icon">
-			<use xlinkHref={`#${props.name}`} />
-		</svg>
-	)
+const Icon: React.FunctionComponent<IconProps> = ({name, className, ...props}) => {
+  const { style, restProps } = useStyle((props as IconProps))
+
+  return (
+    // https://stackoverflow.com/questions/26815738/svg-use-tag-and-reactjs
+    <svg className={classNames('e-ui-icon', className)} {...restProps} style={style}>
+      <use xlinkHref={`#${name}`}/>
+    </svg>
+  )
+}
+
+function useStyle (props: IconProps) {
+  let { style = {}, color, size, ...restProps } = props
+
+  if (color) style.fill = color
+  if (size) style.fontSize = size
+
+  return {
+    style,
+    restProps
+  }
 }
 
 export default Icon
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Icon from "./Icon"
 
-ReactDOM.render(<Icon name="delete"/>, document.getElementById("root"));
+ReactDOM.render(<Icon name="delete" />, document.getElementById("root"));
