7f1aabd69ab55ae2aafb61072f1af60da28be87f
qiqf <723469354@qq.com>
2021-08-14 23:01:22 +0800

    chore: 对项目目录的更改

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Icon from "./Icon"
+import Icon from "./Icon";
 
 ReactDOM.render(<Icon name="delete" />, document.getElementById("root"));
