1fe338e9a5fee42273ca00d529fa516a49e26fc6
qiqf <723469354@qq.com>
2021-08-15 21:08:07 +0800

    test: 对Icon组件添加测试用例

--- lib/Icon/__tests__/icon.unit.tsx
+++ lib/Icon/__tests__/icon.unit.tsx
@@ -1,10 +1,36 @@
 import React from "react";
 import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
 import Icon from "../index";
 
 describe("Icon Component", () => {
-  it("test Icon1", () => {
+  it("Icon Render", () => {
+    const tree = renderer.create(<Icon name="delete" />);
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("Icon Props.name", () => {
     const wrapper = shallow(<Icon name="delete" />);
-    expect(wrapper.find(".e-ui-icon")).toHaveLength(1);
+    const result = wrapper.find("use").props();
+    expect(result.xlinkHref).toBe("#delete");
+  });
+
+  it("Icon Props.color", () => {
+    const wrapper = shallow(<Icon name="delete" color="#f00" />);
+    const result = wrapper.find(".e-ui-icon").props();
+    expect(result.style?.fill).toBe("#f00");
+  });
+
+  it("Icon Props.size", () => {
+    const wrapper = shallow(<Icon name="delete" size={40} />);
+    const result = wrapper.find(".e-ui-icon").props();
+    expect(result.style?.fontSize).toBe(40);
+  });
+
+  it("Icon click", () => {
+    const fn = jest.fn();
+    const wrapper = shallow(<Icon name="delete" onClick={fn} />);
+    wrapper.simulate("click");
+    expect(fn).toBeCalledTimes(1);
   });
 });
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,4 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Icon from "./Icon";
 
-ReactDOM.render(<Icon name="delete" />, document.getElementById("root"));
+ReactDOM.render(
+  <Icon name="delete" color="#f00" />,
+  document.getElementById("root")
+);
